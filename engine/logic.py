b0cc6560d2c7bfaec88b96f5a56d6241e65324ed
Hermes Passer <dsldouglas@hotmail.com>
2018-11-06 20:18:53 -0200

    intern commands exit and help added
    intern commands exit and help added"

--- engine/logic.py
+++ engine/logic.py
@@ -3,7 +3,6 @@ from console import Console, str_to_array
 
 # IMPORTANTE: no lugar de definir as palavras no xml, definir palavras padrão e só definir o que cada uma fará em todos os 
 
-# concatenar comandos expecificos com os de configuração como 'sair'
 
 # customizar futuro vocabulario de erro deixando ele como uma var static ou talvez deixarndo clusula no storyboard?	
 
@@ -13,7 +12,11 @@ WRITE_KEYWORD = 'write'
 WRITEDESC_KEYWORD = 'writedesc'
 GOTO_KEYWORD = 'goto'
 END_KEYWORD = 'end'
+EXIT_INTERN = 'exit' # _exit in the yaml
+HELP_INTERN = 'help' # _help in the yaml and _helptext for the text to be displayed
 # LOADFILE = 'load' # not in use, to load a new storyboard
+
+# acusa '... não é um verbo conhecido' se este verbo não ser usado na cena atual
 
 # DESCRIPTION = 'description'
 
@@ -37,15 +40,20 @@ class Parser:
 			self._load_section(self.curr_sec)
 
 	def _load_default_actions(self):
+		global HELP_INTERN, EXIT_INTERN
 		self._actions = self.yaml_obj['actions']
-		
+		# load the intern_actions
+		HELP_INTERN = self._actions.get('_help', None) or HELP_INTERN
+		EXIT_INTERN = self._actions.get('_exit', None) or EXIT_INTERN
+
 	def _load_section(self, str_section):
 		str_description = self.yaml_obj[str_section].get('description', "...") # show '...' if no description is found
-		Console.writedln(str_description)
+		Console.writedln(str_description.strip())
 		
 		self._load_actions(str_section)
 
 	def _execute_action(self, command, param):
+		global HELP_INTERN, EXIT_INTERN
 		if command == WRITE_KEYWORD:
 			Console.writeln(param)
 		elif command == WRITEDESC_KEYWORD:
@@ -81,13 +89,25 @@ class Parser:
 
 		return (cmd, str_param, found)
 
+	def _exec_intern(self, command):
+		global HELP_INTERN, EXIT_INTERN
+
+		if command == EXIT_INTERN:
+			self.return_load_section = True
+			self._running = False
+			return True
+		elif command == HELP_INTERN:
+			Console.writedln(".ola ...")#self._actions.get('_helptext', ''))
+			return True
+		return False
+
 	def _load_actions(self, section):
 		action_keys = list(self.yaml_obj[section].keys())
 		str_inputargs = Console.scan_args()
 		while True:
 			write_notfound = True
 
-			for action_key in action_keys:
+			for action_key in action_keys:# or action_key:
 				# Maybe make this one optional (check if the key is on the denfineds) (it will need to check if the key exists on the _parse_action)
 				if action_key in self._actions: # check if is a valid key
 					if str_inputargs[0] == action_key: # the user get the correct verb
@@ -95,10 +115,13 @@ class Parser:
 						
 						write_notfound = False
 						if not found: # command will be None if the text after the verb not match
-							Console.writeln(param)
+							Console.writeln(param + "....")
 						else:
 							self._execute_action(cmd, param)
 			
+			if self._exec_intern(str_inputargs[0]): # check and execute if is a intern cmd, return false if not
+				write_notfound = False
+						
 			if self.return_load_section:
 				return
 
