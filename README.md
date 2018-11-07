7137c74ae083bc03d6aa0fb3eb3f91d832ec7156
Hermes Passer <dsldouglas@hotmail.com>
2018-11-07 21:17:00 -0200

    readme updated and game name

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# Castelo Dracula
-A simple Interactive Fiction that adapts the first chapter of Dracula, of Bran Stroker. The game is in portuguese.
+# Harker
+A simple Interactive Fiction that adapts the first chapter of Bram Stoker's Dracula. The game is in portuguese.
 
 # Requirements
  - Python 3.7.1
- - PyYaml 3.13 **[1]**
+ - PyYaml 3.13 *[1]*
 
-**[1]: use 'pip install pyyaml' to install (require python to work)**
+*[1]: use 'pip install pyyaml' to install (require python to work)*
--- main.py
+++ main.py
@@ -5,19 +5,19 @@ from logic import Parser
 STORYBOARD_PATH = 'storyboard.yaml'
  
 def title():
-	Console.writeln(""" _   _       _ _                                   _       _                 _ """)
-	Console.writeln("""| | | | __ _| | | _____      _____  ___ _ __      / \   __| |_   _____ _ __ | |_ _   _ _ __ ___""")
-	Console.writeln("""| |_| |/ _` | | |/ _ \ \ /\ / / _ \/ _ \ '_ \    / _ \ / _` \ \ / / _ \ '_ \| __| | | | '__/ _ \ """)
-	Console.writeln("""|  _  | (_| | | | (_) \ V  V /  __/  __/ | | |  / ___ \ (_| |\ V /  __/ | | | |_| |_| | | |  __/""")
-	Console.writeln("""|_| |_|\__,_|_|_|\___/ \_/\_/ \___|\___|_| |_| /_/   \_\__,_| \_/ \___|_| |_|\__|\__,_|_|  \___|""")
-	Console.writeln('\n 2018 - Hermes Passer')
+	Console.writeln("""  _   _            _""")
+	Console.writeln(""" | | | | __ _ _ __| | _____ _ __""")
+	Console.writeln(""" | |_| |/ _` | '__| |/ / _ \ '__|""")
+	Console.writeln(""" |  _  | (_| | |  |   <  __/ |""")
+	Console.writeln(""" |_| |_|\__,_|_|  |_|\_\___|_|""")
+	Console.writeln('\n 2018 - Hermes Passer (gladiocitrico.blogspot.com)')
 	Console.writeln("\nDigite 'iniciar' para começar o jogo ou 'ajuda' para outras ações.")
 	Console.writeln("Ao clicar com o mouse o jogo pode ser pausado, tecle enter para ele voltar.")
      
 def main():
 	title()
 	while(True):
-		arg = Console.scan()[0]
+		arg = Console.scan_args()[0]
 		 
 		if (arg == 'iniciar'):
 			Console.scroll_down()
